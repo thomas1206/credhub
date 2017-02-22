5e1338c7c2eaeea3517f51b97dbe630be8572cb8
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-02-21 17:05:07 -0800

    WIP
    [#140174145] Default basic auth user's password gets logged
    
    Signed-off-by: Phil Goodwin <pgoodwin@pivotal.io>

--- src/test/java/io/pivotal/security/config/AuthConfigurationTest.java
+++ src/test/java/io/pivotal/security/config/AuthConfigurationTest.java
@@ -6,6 +6,7 @@ import io.pivotal.security.util.DatabaseProfileResolver;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.lang.reflect.Field;
@@ -22,14 +25,14 @@ import java.lang.reflect.Field;
 import static com.greghaskins.spectrum.Spectrum.beforeEach;
 import static com.greghaskins.spectrum.Spectrum.it;
 import static io.pivotal.security.helper.SpectrumHelper.wireAndUnwire;
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 @RunWith(Spectrum.class)
 @ActiveProfiles(value = {"unit-test", "OAuth2ConfigurationTest"}, resolver = DatabaseProfileResolver.class)
 @SpringBootTest
-public class OAuth2ConfigurationTest {
+public class AuthConfigurationTest {
 
   @Autowired
   WebApplicationContext applicationContext;
@@ -38,23 +41,35 @@ public class OAuth2ConfigurationTest {
   SecurityAutoConfiguration securityAutoConfiguration;
 
   @Autowired
-  OAuth2Configuration oAuth2Configuration;
+  AuthConfiguration authConfiguration;
 
   @Autowired
   JwtAccessTokenConverter accessTokenConverter;
+
+  @Autowired
+  SecurityProperties securityProperties;
+
+  @Autowired
+  WebApplicationContext webApplicationContext;
+
+  private MockMvc mockMvc;
 
   {
     wireAndUnwire(this, false);
 
     beforeEach(() -> {
-      oAuth2Configuration.init();
+      mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     });
 
+//    it("should not allow the default user to access protected resources", () -> {
+//      mockMvc.perform(get("/api/v1/data?name=test").principal(securityProperties.getUser()));
+//      securityProperties.getUser();
+//    });
+
     it("should have obscure user populated", () -> {
-      oAuth2Configuration.init();
-      assertThat(oAuth2Configuration.securityProperties.getUser().getName().length(), equalTo(12));
-      assertThat(oAuth2Configuration.securityProperties.getUser().getName(), not(equalTo("user")));
-      assertThat(oAuth2Configuration.securityProperties.getUser().getRole().size(), equalTo(0));
+      assertThat(authConfiguration.securityProperties.getUser().getName().length(), equalTo(12));
+      assertThat(authConfiguration.securityProperties.getUser().getName(), not(equalTo("user")));
+      assertThat(authConfiguration.securityProperties.getUser().getRole().size(), equalTo(0));
     });
 
     it("should be configured to have basic auth disabled", () -> {
